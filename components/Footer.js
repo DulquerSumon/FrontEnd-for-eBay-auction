059eecc7650305c7bc9b104a8adf08a7b8777f95
DulquerSumon <m.dulquer.sumon@gmail.com>
2022-11-18 22:33:13 +0600

    initial commit

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer className="flex absolute bottom-1 w-full h-9 border-t-2 bg-gray-400">
+      <a
+        href="https://goerli.etherscan.io/address/0xf4f3E9F19Aa568848e145855416c205D4bA3Bfe0#code"
+        className="border-b-2 mt-2 border-blue-500"
+      >
+        Click to View on Goerli Etherscan
+      </a>
+      <div className="ml-auto py-34 px-40">© Developed By:</div>
+      <a className=" mr-auto py-34 px-40">Md Sumon</a>
+    </footer>
+  );
+}
